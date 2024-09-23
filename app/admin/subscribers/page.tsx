1faf036d500f575968c5d37b1203451f6ce47bbd
Revmagi <Revmagi@users.noreply.github.com>
2024-09-23 17:02:30 -0400

    added admin user and subscriber placeholder layout pages

--- app/admin/subscribers/page.tsx
+++ app/admin/subscribers/page.tsx
@@ -1,9 +1,102 @@
-import React from 'react';
+
+import {
+
+  MoreHorizontal,
+
+} from "lucide-react"
+import { Badge } from "@/components/ui/badge"
+
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  DropdownMenu,
+
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 
 const Subscribers = () => {
   return (
-    <div className='flex items-center'>
-        <h1 className='text-lg font-semibold md:2xl'>Subscribers</h1></div>
+   
+    <Card x-chunk="dashboard-06-chunk-0">
+    <CardHeader>
+      <CardTitle>Subscribers</CardTitle>
+      <CardDescription>
+        Manage your subscribers and view their details.
+      </CardDescription>
+    </CardHeader>
+    <CardContent>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead className="hidden md:table-cell">
+              Created at
+            </TableHead>
+            <TableHead>
+              <span className="sr-only">Actions</span>
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">
+              Laser Lemonade Machine
+            </TableCell>
+            <TableCell>
+              <Badge variant="default">Active</Badge>
+            </TableCell>
+            <TableCell>$79</TableCell>
+            <TableCell className="hidden md:table-cell">
+              2023-07-12 10:42 AM
+            </TableCell>
+            <TableCell>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    aria-haspopup="true"
+                    size="icon"
+                    variant="ghost"
+                  >
+                    <MoreHorizontal className="h-4 w-4" />
+                    <span className="sr-only">Toggle menu</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                  <DropdownMenuItem>Cancel</DropdownMenuItem>
+
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+
+        </TableBody>
+      </Table>
+    </CardContent>
+  </Card>
+
   );
 };
 
